3fd949485632e6d9295a9348ffc168498b95edca
tangning <love88866xi@qq.com>
2015-12-02 16:18:55 +0800

    Add Opencv codes of HistogramProcessing

---
+++ HistogramProcessing/Opencv/LocalHistogramEqualization/CMakeFiles/LocalHistogramEqualization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LocalHistogramEqualization.dir/main.cpp.o"
+  "LocalHistogramEqualization.pdb"
+  "LocalHistogramEqualization"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LocalHistogramEqualization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
